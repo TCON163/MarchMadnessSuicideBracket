3bac4c758d36bd76ae7bff07b4463c2d5e57565a
TCON163 <tylerconnercareersearch@gmail.com>
2021-12-09 17:13:50 -0600

    set up the spring app for the tourney service

---
+++ Backend/Tourney/src/test/java/com/tcontechco/Tourney/TourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcontechco.Tourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- Backend/User/src/main/java/com/tcontechco/User/controllers/PlayerController.java
+++ Backend/User/src/main/java/com/tcontechco/User/controllers/PlayerController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/Player")
 public class PlayerController {
-    private PlayerService playerService;
+    private final PlayerService playerService;
 
     @Autowired
     public PlayerController(PlayerService playerService){
