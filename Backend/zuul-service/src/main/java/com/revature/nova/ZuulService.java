d73eef5f90f44c79ac8de51126fbc525dc5d2ddf
TCON163 <tylerconnercareersearch@gmail.com>
2021-12-09 14:08:40 -0600

    set up the discovery and zuul service for the backend

---
+++ Backend/zuul-service/src/main/java/com/revature/nova/ZuulService.java
@@ -0,0 +1,16 @@
+package com.revature.nova;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ZuulService {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulService.class, args);
+	}
+}
