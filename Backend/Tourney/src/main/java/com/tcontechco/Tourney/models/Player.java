52edb2a465f4c769be9dc1b4ed1882aee8c69dea
TCON163 <tylerconnercareersearch@gmail.com>
2021-12-09 18:05:56 -0600

    set up the models for the tourney service

---
+++ Backend/Tourney/src/main/java/com/tcontechco/Tourney/models/Player.java
@@ -0,0 +1,43 @@
+package com.tcontechco.Tourney.models;
+
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Player {
+
+    @Id
+    @Column(name = "playerId")
+    private Integer playerId;
+
+    @NonNull
+    @Column(name = "first_name")
+    private String firstName;
+
+    @NonNull
+    @Column(name = "last_name")
+    private String lastName;
+
+    @NonNull
+    @Column(name = "username", unique = true)
+    private String username;
+
+    @NonNull
+    @Column(name = "password")
+    private String password;
+
+    @NonNull
+    @Column
+    private String email;
+
+}
---
+++ Backend/Tourney/src/main/java/com/tcontechco/Tourney/models/TourneyPlayer.java
@@ -0,0 +1,41 @@
+package com.tcontechco.Tourney.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class TourneyPlayer {
+
+    @Id
+    @Column(name = "tourneyPlayerId")
+    private Integer tourneyPlayerId;
+
+    @OneToOne
+    @MapsId
+    private Player player;
+
+    @OneToOne
+    @MapsId
+    private Tourney tourney;
+
+
+    // Currently, using a list to hold the picks of the user, but probably
+    // should look into a set. So, that they can't add the same Team twice.
+    @ManyToOne
+    private List<Team> picks;
+
+    @Column
+    private Boolean alive;
+
+
+}
