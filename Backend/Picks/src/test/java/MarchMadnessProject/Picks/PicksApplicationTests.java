bd862ff9f4eaafe96be02703d432d2c4692399fb
andrewdpetersen <andrew.petersen@revature.net>
2021-12-10 09:24:01 -0600

    added models for picks service

---
+++ Backend/Picks/src/test/java/MarchMadnessProject/Picks/PicksApplicationTests.java
@@ -0,0 +1,13 @@
+package MarchMadnessProject.Picks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
