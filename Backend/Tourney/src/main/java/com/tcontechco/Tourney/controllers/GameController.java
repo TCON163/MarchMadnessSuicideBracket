d8f39695f6d793a571351028dc0424c66f9ea1bd
TCON163 <tylerconnercareersearch@gmail.com>
2021-12-12 05:15:16 -0600

    finished the bracket and simulations for the bracket

--- Backend/Tourney/src/main/java/com/tcontechco/Tourney/controllers/GameController.java
+++ Backend/Tourney/src/main/java/com/tcontechco/Tourney/controllers/GameController.java
@@ -55,6 +55,30 @@ public class GameController {
         return ResponseEntity.ok("2nd round of games are in the database.");
     }
 
+    @GetMapping("/games/fillSweet16")
+    public ResponseEntity<String> fillSweet16(){
+        gameService.fillSweet16();
+        return ResponseEntity.ok("Sweet 16 are in the database.");
+    }
+
+    @GetMapping("/games/fillElite8")
+    public ResponseEntity<String> fillElite8(){
+        gameService.fillElite8();
+        return ResponseEntity.ok("Elite 8 are in the database.");
+    }
+
+    @GetMapping("/games/fillFinal4")
+    public ResponseEntity<String> fillFinal4(){
+        gameService.fillFinalFour();
+        return ResponseEntity.ok("Final 4 are in the database.");
+    }
+
+    @GetMapping("/games/fillChamp")
+    public ResponseEntity<String> fillChamp(){
+        gameService.fillChamp();
+        return ResponseEntity.ok("Champ game is in the database.");
+    }
+
     @GetMapping("/games/simulateFirstRound")
     public ResponseEntity<String> simulateFirst(){
         gameService.simulate1stRound();
@@ -65,6 +89,29 @@ public class GameController {
     public ResponseEntity<String> simulateSecond(){
         gameService.simulate2ndRound();
         return ResponseEntity.ok("simulated 2nd round");
+    }
+
+    @GetMapping("/games/simulateSweet16")
+    public ResponseEntity<String> simulateSweet16(){
+        gameService.simulateSweet16();
+        return ResponseEntity.ok("simulated Sweet16");
+    }
+
+    @GetMapping("/games/simulateElite8")
+    public ResponseEntity<String> simulateElite8(){
+        gameService.simulateElite8();
+        return ResponseEntity.ok("simulated Elite8");
+    }
+    @GetMapping("/games/simulateFinal4")
+    public ResponseEntity<String> simulateFinal4(){
+        gameService.simulateFinalFour();
+        return ResponseEntity.ok("simulated Final4");
+    }
+
+    @GetMapping("/games/simulateChamp")
+    public ResponseEntity<String> simulateChamp(){
+        gameService.simulateChamp();
+        return ResponseEntity.ok("simulated Champ");
     }
 
     @PutMapping("/games/{gameId}/winner/{teamId}")
--- Backend/Tourney/src/main/java/com/tcontechco/Tourney/services/GameService.java
+++ Backend/Tourney/src/main/java/com/tcontechco/Tourney/services/GameService.java
@@ -58,10 +58,10 @@ public class GameService {
                 Team away = teamService.getTeamById(16-k+count);
                 Game game = new Game();
                 if(i<2){
-                    Date date = new Date(122,2,15);
+                    Date date = new Date(122,2,17);
                     game.setDate(date);
                 } else {
-                    Date date = new Date(122,2,16);
+                    Date date = new Date(122,2,18);
                     game.setDate(date);
                 }
 
@@ -95,10 +95,10 @@ public class GameService {
 
                 Game game = new Game();
                 if(i<2){
-                    Date date = new Date(122,2,17);
+                    Date date = new Date(122,2,19);
                     game.setDate(date);
                 } else {
-                    Date date = new Date(122,2,18);
+                    Date date = new Date(122,2,20);
                     game.setDate(date);
                 }
 
@@ -118,23 +118,140 @@ public class GameService {
         }
     }
 
+    public void fillSweet16(){
+        int count = 33;
+        Integer countId = 49;
+        for(int i = 0; i<8; i++){
+            Game low = gameRepo.getById(count);
+            Game high = gameRepo.getById(count+1);
+            Team home = low.getWinner();
+            Team away = high.getWinner();
+
+            Game game = new Game();
+            if(i<4){
+                Date date = new Date(122,2,24);
+                game.setDate(date);
+            } else {
+                Date date = new Date(122,2,25);
+                game.setDate(date);
+            }
+
+            Time time = new Time(12,0,0);
+            game.setCompleted(false);
+            game.setHome(home);
+            game.setAway(away);
+
+            game.setTime(time);
+            game.setGameId(countId);
+            gameRepo.save(game);
+
+            count+=2;
+            countId++;
+        }
+
+    }
+
+
+    public void fillElite8(){
+        int count = 49;
+        Integer countId = 57;
+        for(int i = 0; i<4;  i++){
+            Game low = gameRepo.getById(count);
+            Game high = gameRepo.getById(count+1);
+            Team home = low.getWinner();
+            Team away = high.getWinner();
+
+            Game game = new Game();
+            if(i<2){
+                Date date = new Date(122,2,26);
+                game.setDate(date);
+            } else {
+                Date date = new Date(122,2,27);
+                game.setDate(date);
+            }
+
+            Time time = new Time(12,0,0);
+            game.setCompleted(false);
+            game.setHome(home);
+            game.setAway(away);
+
+            game.setTime(time);
+            game.setGameId(countId);
+            gameRepo.save(game);
+
+            count+=2;
+            countId++;
+        }
+    }
+
+    public void fillFinalFour(){
+        Game fiftyseven = gameRepo.getById(57);
+        Game fiftyeight = gameRepo.getById(58);
+        Team west = fiftyseven.getWinner();
+        Team east = fiftyeight.getWinner();
+        Game sixty1 = new Game();
+        Date date = new Date(122,3,2);
+        Time time = new Time(12,0,0);
+        sixty1.setCompleted(false);
+        sixty1.setHome(west);
+        sixty1.setAway(east);
+        sixty1.setTime(time);
+        sixty1.setDate(date);
+        sixty1.setGameId(61);
+        gameRepo.save(sixty1);
+
+        Game fifty9 = gameRepo.getById(59);
+        Game sixty = gameRepo.getById(60);
+        Team south = fifty9.getWinner();
+        Team midWest = sixty.getWinner();
+        Game sixty2 = new Game();
+        sixty2.setCompleted(false);
+        sixty2.setHome(south);
+        sixty2.setAway(midWest);
+        sixty2.setTime(time);
+        sixty2.setDate(date);
+        sixty2.setGameId(62);
+        gameRepo.save(sixty2);
+    }
+
+    public void fillChamp(){
+        Game west = gameRepo.getById(61);
+        Game east = gameRepo.getById(62);
+        Team home = west.getWinner();
+        Team away = east.getWinner();
+        Game game = new Game();
+        Date date = new Date(122,3,4);
+        Time time = new Time(12,0,0);
+        game.setCompleted(false);
+        game.setHome(home);
+        game.setAway(away);
+        game.setDate(date);
+        game.setTime(time);
+        game.setGameId(63);
+        gameRepo.save(game);
+    }
+
     public void simulate1stRound(){
         for(int i=1; i<33;i++){
             Random random = new Random();
             int upperbound=21;
             int randomInt = random.nextInt(upperbound);
             if (randomInt%2==0){
-
                 Game game = gameRepo.getById(i);
                 Team winner = game.getHome();
+                Team loser = game.getAway();
+                loser.setAlive(false);
+                teamService.createTeam(loser);
                 game.setWinner(winner);
                 game.setCompleted(true);
-
                 gameRepo.save(game);
 
             }else{
                 Game game = gameRepo.getById(i);
                 Team winner = game.getAway();
+                Team loser = game.getHome();
+                loser.setAlive(false);
+                teamService.createTeam(loser);
                 game.setWinner(winner);
                 game.setCompleted(true);
                 gameRepo.save(game);
@@ -152,16 +269,151 @@ public class GameService {
             if(rand%2==0){
                 Game game = gameRepo.getById(i);
                 Team winner = game.getHome();
+                Team loser = game.getAway();
+                loser.setAlive(false);
+                teamService.createTeam(loser);
                 game.setWinner(winner);
                 game.setCompleted(true);
                 gameRepo.save(game);
             }else{
                 Game game = gameRepo.getById(i);
                 Team winner = game.getAway();
+                Team loser = game.getHome();
+                loser.setAlive(false);
+                teamService.createTeam(loser);
                 game.setWinner(winner);
                 game.setCompleted(true);
                 gameRepo.save(game);
             }
         }
     }
+
+
+    public void simulateSweet16(){
+        for(int i = 49; i<57; i++){
+            Random random = new Random();
+            int up = 21;
+            int rand = random.nextInt(21);
+            if(rand%2==0){
+                Game game = gameRepo.getById(i);
+                Team winner = game.getHome();
+                Team loser = game.getAway();
+                loser.setAlive(false);
+                teamService.createTeam(loser);
+                game.setWinner(winner);
+                game.setCompleted(true);
+                gameRepo.save(game);
+            }else{
+                Game game = gameRepo.getById(i);
+                Team winner = game.getAway();
+                Team loser = game.getHome();
+                loser.setAlive(false);
+                teamService.createTeam(loser);
+                game.setWinner(winner);
+                game.setCompleted(true);
+                gameRepo.save(game);
+            }
+        }
+    }
+
+    public void simulateElite8(){
+        for(int i = 57; i<61;i++){
+            Random random = new Random();
+            int up = 21;
+            int rand = random.nextInt(21);
+            if(rand%2==0){
+                Game game = gameRepo.getById(i);
+                Team winner = game.getHome();
+                Team loser = game.getAway();
+                loser.setAlive(false);
+                teamService.createTeam(loser);
+                game.setWinner(winner);
+                game.setCompleted(true);
+                gameRepo.save(game);
+            }else{
+                Game game = gameRepo.getById(i);
+                Team winner = game.getAway();
+                Team loser = game.getHome();
+                loser.setAlive(false);
+                teamService.createTeam(loser);
+                game.setWinner(winner);
+                game.setCompleted(true);
+                gameRepo.save(game);
+            }
+        }
+    }
+
+
+
+    public void simulateFinalFour(){
+        Random random = new Random();
+        int rand = random.nextInt(21);
+        if(rand%2==0){
+            Game game = gameRepo.getById(61);
+            Team winner = game.getHome();
+            Team loser = game.getAway();
+            loser.setAlive(false);
+            teamService.createTeam(loser);
+            game.setWinner(winner);
+            game.setCompleted(true);
+            gameRepo.save(game);
+        }else{
+            Game game = gameRepo.getById(61);
+            Team winner = game.getAway();
+            Team loser = game.getHome();
+            loser.setAlive(false);
+            teamService.createTeam(loser);
+            game.setWinner(winner);
+            game.setCompleted(true);
+            gameRepo.save(game);
+        }
+
+        Random random2 = new Random();
+        int rand2 = random2.nextInt(21);
+        if(rand2%2==0){
+            Game game = gameRepo.getById(62);
+            Team winner = game.getHome();
+            Team loser = game.getAway();
+            loser.setAlive(false);
+            teamService.createTeam(loser);
+            game.setWinner(winner);
+            game.setCompleted(true);
+            gameRepo.save(game);
+        }else{
+            Game game = gameRepo.getById(62);
+            Team winner = game.getAway();
+            Team loser = game.getHome();
+            loser.setAlive(false);
+            teamService.createTeam(loser);
+            game.setWinner(winner);
+            game.setCompleted(true);
+            gameRepo.save(game);
+        }
+
+    }
+
+    public void simulateChamp(){
+        Random random = new Random();
+        int rand = random.nextInt(21);
+        if(rand%2==0){
+            Game game = gameRepo.getById(63);
+            Team winner = game.getHome();
+            Team loser = game.getAway();
+            loser.setAlive(false);
+            teamService.createTeam(loser);
+            game.setWinner(winner);
+            game.setCompleted(true);
+            gameRepo.save(game);
+        }else{
+            Game game = gameRepo.getById(61);
+            Team winner = game.getAway();
+            Team loser = game.getHome();
+            loser.setAlive(false);
+            teamService.createTeam(loser);
+            game.setWinner(winner);
+            game.setCompleted(true);
+            gameRepo.save(game);
+        }
+    }
+
 }
